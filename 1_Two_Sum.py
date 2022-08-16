ea9fbd566363b1c3dbf35fb0d60e93cd8a2384c2
James Alexander Leak <alex.leak@gmail.com>
2022-08-16 17:51:29 -0500

    1. Two Sum
    Two Sum creates a dictionary to store the values and indices as it loops
    through input nums. When the remainder of target - currentValue is in
    the dictionary, returns that index along with currentIndex. Does not
    return nothing since user can assume all inputs will have a valid
    output.

---
+++ 1_Two_Sum.py
@@ -0,0 +1,24 @@
+from pandas import value_counts
+
+
+class Solution:
+    def twoSum(self, nums: List[int],target: int) -> List[int]:
+        """
+        Given an array of integers nums and an integer taget,
+        return two indices of the two numbers such that they add
+        up to the target. You may assume that each input would have
+        exactly one solution, and you may not use the same element
+        twice.
+        """
+        #create a dictionary to store differences
+        diction = dict()
+        #iterate through nums
+        for index,value in enumerate(nums):
+            #subtract the target from the current value
+            diff = target - value
+            #if the difference is in the dicitonary, return index of the difference and cur index
+            if diff in diction:
+                return [diction[diff],index]
+            #else, store key:value value:index
+            else:
+                diction[value] = index
